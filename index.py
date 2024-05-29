8b4a78012f9d80a2d0d7b1f7be9a581842e708a6
Ederllon <156702239+Ederllon@users.noreply.github.com>
2024-05-28 23:25:54 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import pywhatkit as wpp
 num = int(input('Escreva apenas o DDD e o número de quem irá receber a mensagem (apenas números): '))
-convert = str(num)
+convert = str(num) 
 numConvert = str('+55' + convert)
 msg = str(input('Escreva a mensagem: '))
 hour = int(input('Qual hora vai enviar essa mensagem? '))
